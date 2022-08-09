080c8964d2971a03c1996d731c44119de4dc203f
ganzidaeyong <iwin1203@naver.com>
2022-08-09 23:11:24 +0900

    20220809 / practice / solidity

---
+++ contracts/Primitives.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+contract Primitives{
+
+    bool public boo = true;
+    
+    uint8 public u8 = 8;
+    uint256 public u256 = 256;
+    uint public u = 256;
+
+    int8 public i8 = -8;
+    int256 public i256 = -256;
+    int public i = -256;
+
+    int public maxint = type(int).max;
+    int public minint = type(int).min;
+
+    address public addr = 0xCA35b7d915458EF540aDe6068dFe2F44E8fa733c;
+
+    bytes1 
+
+}
