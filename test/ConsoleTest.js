b65dbc20405ab3a918631169d53e488c5b1919cd
ganzidaeyong <iwin1203@naver.com>
2022-08-23 00:26:33 +0900

    feat / describe it을 function으로 wrapping 실패. 다른 방법 찾기

--- test/ConsoleTest.js
+++ test/ConsoleTest.js
@@ -1,19 +1,39 @@
 const { ethers } = require("hardhat");
 const { expect } = require("chai");
 const fs = require("fs").promises;
-const solutionPath = "./tc&sol/";
 
-describe("test", () => {
-  it("print function test", async () => {
+describe("TEST BEGAN", () => {
+  it("deployed", async () => {
     const PS_factory = await ethers.getContractFactory("PS");
     const deployed = await PS_factory.deploy();
 
-    let answer = await fs.readFile(`${solutionPath}sol01`);
-    answer = answer.toString().trim();
+    const [tcList, solList] = await getDir();
 
-    let tc = await fs.readFile(`${solutionPath}tc01`);
-    tc = tc.toString().trim();
+    for (i = 0; i < tcList.length; i++) {
+      console.log("came");
 
-    expect(await deployed.exec(tc)).to.equal(answer);
+      let answer = await fs.readFile(solList[i]);
+      answer = answer.toString().trim();
+
+      let tc = await fs.readFile(tcList[i]);
+      tc = tc.toString().trim();
+
+      console.log("This is Sol -> ", answer);
+      console.log("This is TC -> ", tc);
+
+      expect(await deployed.exec(tc)).to.equal(answer);
+    }
   });
 });
+
+const getDir = async () => {
+  const tcPath = "./tc&sol/tc/";
+  const solPath = "./tc&sol/sol/";
+  let tcList = await fs.readdir(tcPath);
+  let solList = await fs.readdir(solPath);
+  for (i = 0; i < tcList.length; i++) {
+    tcList[i] = tcPath + tcList[i];
+    solList[i] = solPath + solList[i];
+  }
+  return [tcList, solList];
+};
